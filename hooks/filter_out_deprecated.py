5a50cbb1bce0b129f9080c5d827e6a0869f94fc1
jesseemond-adsk <Jesse.Emond@Autodesk.com>
2015-10-08 10:19:08 -0400

    For #14, Control Deprecated Filtering With Setting
    Modify filter_out_deprecated hook to check a `show_deprecated_files`
    user setting to decide whether to filter out deprecated files.
    
    Add a shotgun-utils dependency to access user settings.
    
    [minor]
    Make the filter PEP8 compliant.
    
    Closes #4.

--- hooks/filter_out_deprecated.py
+++ hooks/filter_out_deprecated.py
@@ -1,41 +1,56 @@
 # Copyright (c) 2015 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import os
-
-import sgtk
 from sgtk import Hook
+
 
 class FilterPublishes(Hook):
     """
-    Hook that can be used to filter the list of publishes returned from Shotgun for the current
-    location
+    Hook that can be used to filter the list of publishes returned from
+    Shotgun for the current location.
     """
-    
+
     def execute(self, publishes, **kwargs):
         """
         Main hook entry point
-        
-        :param publishes:    List of dictionaries 
-                             A list of  dictionaries for the current location within the app.  Each
-                             item in the list is a Dictionary of the form:
-                             
+
+        :param publishes:    List of dictionaries
+                             A list of  dictionaries for the current location
+                             within the app.  Each item in the list is a
+                             Dictionary of the form:
+
                              {
-                                 "sg_publish" : {Shotgun entity dictionary for a Published File entity}
+                                 "sg_publish" : {Shotgun entity dictionary for
+                                                 a Published File entity}
                              }
-                             
-                                                         
-        :returns:            The filtered list of dictionaries of the same form as the input 'publishes'
-                             list
+
+
+        :returns:            The filtered list of dictionaries of the same form
+                             as the input 'publishes' list
         """
-        framework = self.load_framework("tk-framework-simple_v0.x.x")
-        utils = framework.import_module("utils")
-        return [publish for publish in publishes
-                if not utils.is_deprecated(publish["sg_publish"])]
+        app = self.parent
+        shotgun_utils = self.load_framework(
+            "tk-framework-shotgunutils_v2.x.x")
+        settings = shotgun_utils.import_module("settings")
+        settings_manager = settings.UserSettings(app)
+
+        # This user setting is expected to be set by the app using this hook
+        # (e.g. the loader) to control whether to show the deprecated files or
+        # filter them out.
+        show_deprecated = settings_manager.retrieve(
+            "show_deprecated_files", False)
+
+        if not show_deprecated:
+            framework = self.load_framework("tk-framework-simple_v0.x.x")
+            utils = framework.import_module("utils")
+            publishes = [publish for publish in publishes
+                         if not utils.is_deprecated(publish["sg_publish"])]
+
+        return publishes
